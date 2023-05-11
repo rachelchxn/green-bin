1f3cadfb51a5c51422cc6e9e1e7232e505d4e85f
rachelchxn <85042944+rachelchxn@users.noreply.github.com>
2023-05-10 22:38:47 -0400

    welcome back john

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, Button, Pressable } from 'react-native';
+import { useState } from 'react';
+// import { Stack, useRouter } from 'expo-router';
+
 
 export default function App() {
+
+  // const router = useRouter();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.topSection}>
+          <Text style={styles.heading1}>Welcome back, John.</Text>  
+        </View>
+        <View style={styles.mainSection}>
+          <Text style={styles.heading2}>Sorting Waste</Text>  
+          <Pressable style={styles.card}>
+            <Text>TAP HERE</Text>
+          </Pressable>
+          <Pressable style={styles.card}>
+            <Text>TAP HERE</Text>
+          </Pressable>
+          <Pressable style={styles.card}>
+            <Text>TAP HERE</Text>
+          </Pressable>
+        </View>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F3F6F5',
+    fontFamily: 'Inter'
   },
+  topSection: {
+    backgroundColor: '#216D60',
+    paddingTop: 80,
+    padding: 20
+  },
+  mainSection: {
+    padding: 20
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderColor: '#D9E4E0',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginTop: 20
+  },
+  heading1: {
+    fontSize: 32,
+    color: '#fff',
+    fontWeight: 600
+  },
+  heading2: {
+    fontSize: 20,
+    fontWeight: 500
+  }
 });
